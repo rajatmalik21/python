8e2c0e834feee5056b80c45bc4d778f9322f56f9
Rajat <rajat_malik@hotmail.com>
2020-05-04 11:26:44 +0530

    String printing

---
+++ 6StringPrinting.py
@@ -0,0 +1,8 @@
+str='Hello World'
+
+print(str)           #will print the entire string
+print(str[0])        #will print the first character of a string
+print(str[2:5])      #will print the 3rd to 5th character of a string
+print(str[2:])       #will print string from 3rd character onwards
+print(str * 2)       #will print string twice
+print(str + "TEST")  #will concatenate strings and print it
