2b8e6914ac0f37da3d3854b0694110bc17272c39
Rajat <rajat_malik@hotmail.com>
2020-05-16 15:35:45 +0530

    added Type Hinting

--- Milestone2_sql/util/database.py
+++ Milestone2_sql/util/database.py
@@ -1,52 +1,42 @@
-import sqlite3
+from typing import List, Dict, Union
 
-books_file = 'books1.json'
+from .database_connection import DatabaseConnection
 
-def create_book_table():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-
-    cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key,author text,read integer)')
-
-    connection.commit()
-    connection.close()
+Book = Dict(str,Union(str,int))]
 
 
-def add_book(name,author):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-
-    cursor.execute('INSERT INTO books VALUES(?, ?, 0)',(name,author))
-
-    connection.commit()
-    connection.close()
+def create_book_table() -> None:
+    with DatabaseConnection('database.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key,author text,read integer)')
 
 
-def get_all_books():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
+def add_book(name: str,author: str) -> None:
+    with DatabaseConnection('database.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('SELECT * FROM books')
-    books =[{'name' : row[0], 'author': row[1], 'read':row[2]} for row in cursor.fetchall()]   #[(name,author,read),(name,author,read),]
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)',(name,author))
 
-    connection.close()
+
+
+def get_all_books() -> List[Book]:
+    with DatabaseConnection('database.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM books')
+        books =[{'name' : row[0], 'author': row[1], 'read':row[2]} for row in cursor.fetchall()]   #[(name,author,read),(name,author,read),]
     return books
 
 
-def mark_book_as_read(name):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
+def mark_book_as_read(name) -> None:
+    with DatabaseConnection('database.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('UPDATE books SET read=1 WHERE name = ?',(name,))
-
-    connection.commit()
-    connection.close()
+        cursor.execute('UPDATE books SET read=1 WHERE name = ?',(name,))
 
 
-def delete_book(name):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
+def delete_book(name:str) -> None:
+    with DatabaseConnection('database.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('DELETE from books WHERE name = ?',name,)
-    connection.commit()
-    connection.close()
+        cursor.execute('DELETE from books WHERE name = ?',(name,))
