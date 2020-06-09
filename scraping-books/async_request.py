8cf1fb802717fbb6b3674137217f710dd26f8578
Rajat <rajat_malik@hotmail.com>
2020-06-09 16:04:23 +0530

    added data

---
+++ scraping-books/async_request.py
@@ -0,0 +1,29 @@
+import aiohttp
+import asyncio
+import time
+import async_timeout
+
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f'Page took {time.time() - page_start} seconds')
+            return response.status
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks =[]
+    async with aiohttp.ClientSession(loop = loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session,url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+loop = asyncio.get_event_loop()
+
+urls = ['http://google.com' for i in range(50)]
+start = time.time()
+loop.run_until_complete(get_multiple_pages(loop,*urls))
+print(f'All took {time.time() - start} seconds')
